7b314a0d266bb0b891df9ea2275ba8cb43f785a4
Roker2 <dimaminko2000@gmail.com>
2020-04-13 16:31:11 +0300

    mainpage: Add YandexMusic model

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Device
+from .models import YandexMusic
 
 # Register your models here.
 
 admin.site.register(Device)
+admin.site.register(YandexMusic)
--- mainpage/models.py
+++ mainpage/models.py
@@ -12,3 +12,17 @@ class Device(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class YandexMusic(models.Model):
+    musicUrlWithGrid = models.URLField()
+    musicUrl = models.URLField()
+    musicName = models.CharField(max_length=50)
+    artistUrl = models.URLField()
+    artistName = models.CharField(max_length=50)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.artistName + ' - ' + self.musicName
