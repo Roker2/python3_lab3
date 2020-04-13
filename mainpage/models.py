de91c0f8ee6b9617151ad79c2f72515a30e5bdfb
Roker2 <dimaminko2000@gmail.com>
2020-04-13 14:35:12 +0300

    mainpage: models: Device: Change type of name to Char Field

--- mainpage/models.py
+++ mainpage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Device(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=50)
     picture = models.ImageField()
     url = models.TextField()
 
