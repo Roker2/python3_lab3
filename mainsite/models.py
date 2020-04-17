e76b675222f2e430c119279a1e86bab5dddf3713
Roker2 <dimaminko2000@gmail.com>
2020-04-17 11:41:46 +0300

    mainsite/models: YandexMusic: Rework

--- mainsite/models.py
+++ mainsite/models.py
@@ -15,11 +15,20 @@ class Device(models.Model):
 
 
 class YandexMusic(models.Model):
-    musicUrlWithGrid = models.URLField()
-    musicUrl = models.URLField()
-    musicName = models.CharField(max_length=50)
-    artistUrl = models.URLField()
     artistName = models.CharField(max_length=50)
+    musicName = models.CharField(max_length=50)
+    trackInt = models.IntegerField(default=0)
+    albumInt = models.IntegerField(default=0)
+    artistInt = models.IntegerField(default=0)
+
+    def musicUrlWithGrid(self) -> str:
+        return 'https://music.yandex.ru/iframe/#track/' + str(self.trackInt) + '/' + str(self.albumInt)
+
+    def musicUrl(self) -> str:
+        return 'https://music.yandex.ru/album/' + str(self.albumInt) + '/track/' + str(self.trackInt)
+
+    def artistUrl(self) -> str:
+        return 'https://music.yandex.ru/artist/' + str(self.albumInt)
 
     def publish(self):
         self.save()
