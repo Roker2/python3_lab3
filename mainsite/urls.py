179cbd5b07aac3c5ade87444a37c1b865bdc199a
Roker2 <dimaminko2000@gmail.com>
2020-04-21 13:40:48 +0300

    mainsite: Add possibility to work with Artist model objecnts

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.mainpage, name='mainpage'),
     path('favoritemusic', views.musicpage, name='musicpage'),
     path('devices', views.devicespage, name='devicespage'),
+    path('artist', views.ArtistList.as_view(), name='artist_list'),
+    path('artist/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
+    path('create', views.ArtistCreate.as_view(), name='artist_create'),
+    path('edit/<int:pk>', views.ArtistUpdate.as_view(), name='artist_edit'),
+    path('delete/<int:pk>', views.ArtistDelete.as_view(), name='artist_delete'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Artist
 from .models import YandexMusic
 from .models import mp3Music
 from .models import mp3LocalMusic
@@ -18,3 +22,33 @@ def musicpage(request):
 
 def devicespage(request):
     return render(request, 'devicespage/devicespage.html', {'devices': Device.objects.all()})
+
+
+class ArtistList(ListView):
+    model = Artist
+    template_name = 'artist_crud/artist_list.html'
+
+
+class ArtistDetail(DetailView):
+    model = Artist
+    template_name = 'artist_crud/artist_details.html'
+
+
+class ArtistCreate(CreateView):
+    model = Artist
+    template_name = 'artist_crud/artist_form.html'
+    fields = ['Name']
+    success_url = reverse_lazy('artist_list')
+
+
+class ArtistUpdate(UpdateView):
+    model = Artist
+    template_name = 'artist_crud/artist_form.html'
+    fields = ['Name']
+    success_url = reverse_lazy('artist_list')
+
+
+class ArtistDelete(DeleteView):
+    model = Artist
+    template_name = 'artist_crud/artist_confirm_delete.html'
+    success_url = reverse_lazy('artist_list')
